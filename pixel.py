8706674b3ea49589b61b4282929628cf20862336
Rick Sorensen <rick.sorensen@gmail.com>
2026-04-06 09:52:31 -0500

    pixel,upy_backend: Docstrings.
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- pixel.py
+++ pixel.py
@@ -1,13 +1,42 @@
+"""Basic pixel properties."""
+
+
 class Pixel:
-    def __init__(self, position):
+    """Pixel properties in an LED strip.
+
+    Parameters
+    ----------
+
+    position : `int`
+       Position in strip for this pixel.
+       sets color to (0,0,0)
+
+    """
+
+    def __init__(self, position: int):
         self.position = position
         self.current_color = (0, 0, 0)
 
-    def update_color(self, new_color):
+    def update_color(self, new_color: tuple[int, int, int]):
+        """Change pixel color.
+
+        Args:
+            new_color: Color 3-tuple R,G,B.
+        """
         self.current_color = new_color
 
-    def get_position(self):
+    def get_position(self) -> int:
+        """Get position in LED strip of pixel.
+
+        Returns:
+            position: int
+        """
         return self.position
 
-    def get_color(self):
+    def get_color(self) -> tuple[int, int, int]:
+        """Get color of this pixel.
+
+        Returns:
+            color: 3-tuple R,G,B.
+        """
         return self.current_color
--- upy_backend.py
+++ upy_backend.py
@@ -1,12 +1,28 @@
+"""
+MicroPython NeoPixel Strip emulation.
+"""
+
 from pixel import Pixel
 from neopixel_emulator import NeoPixel_Emulator
 
 
 class machine:
-    """Dummy implementation if needed"""
+    """Dummy partial implementation of MicroPython machine class.
+
+    For this emulation it implements a `machine.Pin()` class interface.
+       machine.Pin()
+    """
 
     class Pin:
-        def __init__(self, pref):
+        """Dummy partial Pin class.
+
+        Parameter
+        ---------
+
+        pref int : Pin (gpio) number
+        """
+
+        def __init__(self, pref: int):
             self.p = pref
 
     def _init_(self):
@@ -14,8 +30,26 @@ class machine:
 
 
 class NeoPixel:
+    """Emulated MicroPython neopixel.NeoPixel class
+
+    Parameter
+    ---------
+    pixel_pin:  Gpio (pin) number.
+    pixel_num:  Number of pixels in strip.
+    init:       optional whether or not to start emulation display now
+    window_w:   optional Size of neopixel emulation window
+    window_h:   optional
+    pixsize:    optional Size of pixel
+    """
+
     def __init__(
-        self, pixel_pin, pixel_num, init=True, window_w=1765, window_h=400, pixsize=10
+        self,
+        pixel_pin: int,
+        pixel_num: int,
+        init: bool = True,
+        window_w: int = 1765,
+        window_h: int = 400,
+        pixsize: int = 10,
     ):
         self.pixel_number = pixel_num
         self.pin = pixel_pin
@@ -25,7 +59,17 @@ class NeoPixel:
         if init:
             self.begin(window_w=window_w, window_h=window_h)
 
-    def begin(self, width=None, height=None, window_w=1765, window_h=400):
+    def begin(self, width=None, height=None, window_w=1765, window_h=400) -> None:
+        """Start emulated display.
+
+        Starts NeoPixel_Emulator and builds up data structures.
+
+        Args:
+            width: screen width of emulated display
+            height: screen height of emulated display
+            window_w: tbd
+            window_h: tbd
+        """
         self.gui = NeoPixel_Emulator(
             window_w=window_w, window_h=window_h, pixsize=self.pixsize
         )
@@ -39,25 +83,32 @@ class NeoPixel:
         self.gui.render()
         self.started = True
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.pixel_number
 
-    def __setitem__(self, i, v):
+    def __setitem__(self, i: int, v: tuple[int, int, int]):
         self.gui.draw_color(i, v)
         self.pixel_list[i].update_color(v)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int):
         return self.pixel_list[i].get_color()
 
     def write(self):
+        """Write data to emulated display."""
         # for pixel in self.pixel_list:
         # print("Pixel {0} has color {1}".format(pixel.get_position(),pixel.get_color()))
         self.gui.render()
 
-    def fill(self, v):
+    def fill(self, v: tuple[int, int, int]):
+        """Fill emulated display with same color.
+
+        Args:
+            v: R,G,B tuple
+        """
         for i in range(0, self.pixel_number):
             self.gui.draw_color(i, v)
             self.pixel_list[i].update_color(v)
 
     def fclose(self):
+        """Close pyglet display."""
         self.gui.close()
